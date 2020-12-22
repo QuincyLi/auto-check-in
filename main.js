53eb8ee0097dbebca47a0ee4c0c1bb1837d47692
quincyli <lq466217509@gmail.com>
2020-12-22 15:14:12 +0800

    feat: init project

---
+++ main.js
@@ -0,0 +1,61 @@
+auto.waitFor();
+// 唤醒手机
+if (!device.isScreenOn()) {
+  device.wakeUpIfNeeded()
+  // 解锁界面
+  gesture(500, [300, 1500], [300, 300]);
+}
+
+var checkTimeGap = 10 * 60 * 1000;
+function checkTime() {
+  setInterval(function () {
+    const today = new Date();
+    if (today.getHours() >= 17) {
+      click(738, 1200); //  点击签退
+      sleep(6000); // 加载时间
+      click(500, 2118); // 点击提交按钮
+    }
+
+    if (today.getHours() <= 8 && new Date().getHours() > 7) {
+      click(432, 1200); //  点击签退
+      sleep(6000); // 加载时间
+      click(500, 2118); // 点击提交按钮
+    }
+
+    app.launchApp("Auto.js");
+  }, checkTimeGap);
+}
+
+checkTime();
+
+app.launchApp("Auto.js");
+
+app.startActivity({
+  action: "android.intent.action.MAIN",
+  packageName: "com.seeyon.cmp",
+  className: "com.seeyon.cmp.ui.LoadActivity",
+  category: ["android.intent.category.LAUNCHER"],
+  flags: ["activity_new_task"]
+});
+
+sleep(7000);
+
+// 滑动到顶部
+gesture(1000, [300, 300], [300, 1000]);
+
+sleep(1500);
+
+
+var checkInItem = className("android.widget.LinearLayout").depth(14).indexInParent(0).findOnce();
+
+if (checkInItem) {
+  checkInItem.click();
+
+  sleep(3000);
+
+  click(831, 1974); // 点击查看详情
+
+  sleep(9000);
+
+  click(345, 1611); //  点击签到
+}
